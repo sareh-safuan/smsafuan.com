19de3f40f98330bc7d602f67f11281dfd6e9a3a9
Sareh Safuan <sarehsafuan@gmail.com>
2020-05-14 23:17:10 +0800

    working navigation both mobile & desktop

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,38 @@ import "./style.css"
 
 const App = () => {
 
-    const [toggle, toggleMenu] = useState(false)
+    const [toggle, setToggle] = useState(false)
+    const [content, setContent] = useState(<Home />)
 
-    const renderMobileMenus = () => {
-        const transform = toggle ? 'translateX(0)' : 'translateX(100%)'
-        return (
-            <div
-                className="mobile-menus"
-                style={{ transform }}
-            >
-                <span
-                    style={{ fontSize: '24px' }}
-                    onClick={() => {
-                        toggleMenu(!toggle)
-                    }}
-                >
-                    X
-                </span>
-                <ul>
-                    <li>
-                        <button>Home</button>
-                    </li>
-                    <li>
-                        <button>Works</button>
-                    </li>
-                    <li>
-                        <button>Contact</button>
-                    </li>
-                </ul>
-            </div>
-        )
+    const toggleMobileMenu = () => {
+        setToggle(!toggle)
+    }
+
+    const changeContent = (e) => {
+        const el = e.target
+        if (el.tagName === "A") {
+            const id = +el.id.substr(-1)
+
+            switch (id) {
+                case 1:
+                    setContent(<Home />)
+                    break
+                case 2:
+                    setContent(<Works />)
+                    break
+                case 3:
+                    setContent(<Contact />)
+                    break
+                default:
+                    setContent(<Home />)
+                    break
+            }
+        }
+    }
+
+    const mobileChangeContent = (e) => {
+        changeContent(e)
+        toggleMobileMenu()
     }
 
     return (
@@ -41,24 +43,76 @@ const App = () => {
                 <div className="logo">
                     logowashere
                 </div>
-                <div className="menus">
-                    <ul>
-                        <li>home</li>
-                        <li>works</li>
-                        <li>contact</li>
-                    </ul>
-                </div>
-                <div className="toggle-icon" onClick={() => {
-                    toggleMenu(!toggle)
-                }}>
-                    <div className="burger"></div>
-                    <div className="burger"></div>
-                    <div className="burger"></div>
-                </div>
+                <Menus changeContent={changeContent} />
+                <HamburgerButton toggleMobileMenu={toggleMobileMenu} />
             </div>
-            {renderMobileMenus()}
+            <MobileMenus 
+                toggle={toggle} 
+                toggleMobileMenu={toggleMobileMenu}
+                mobileChangeContent={mobileChangeContent}    
+            />
+            {content}
         </Fragment>
     )
 }
+
+const Menus = ({ changeContent }) => {
+    return (
+        <div className="menus">
+            <ul onClick={changeContent}>
+                <li>
+                    <a href="#/home" id="content-1">Home</a>
+                </li>
+                <li>
+                    <a href="#/works" id="content-2">Works</a>
+                </li>
+                <li>
+                    <a href="#/contact" id="content-3">Contact</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+const HamburgerButton = ({ toggleMobileMenu }) => {
+    return (
+        <div className="toggle-icon" onClick={toggleMobileMenu} >
+            <div className="burger"></div>
+            <div className="burger"></div>
+            <div className="burger"></div>
+        </div>
+    )
+}
+
+const MobileMenus = ({ toggle, toggleMobileMenu, mobileChangeContent }) => {
+    const transform = toggle ? 'translateX(0)' : 'translateX(100%)'
+    return (
+        <div className="mobile-menus" style={{ transform }}>
+            <span
+                style={{ fontSize: '24px' }}
+                onClick={toggleMobileMenu}
+            >
+                X
+            </span>
+            <ul onClick={mobileChangeContent}>
+                <li>
+                    <a href="#/home" id="content-1">Home</a>
+                </li>
+                <li>
+                    <a href="#/works" id="content-2">Works</a>
+                </li>
+                <li>
+                    <a href="#/contact" id="content-3">Contact</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+const Home = () => <h2>Home</h2>
+
+const Works = () => <h2>Works</h2>
+
+const Contact = () => <h2>Contact</h2>
 
 export default App
